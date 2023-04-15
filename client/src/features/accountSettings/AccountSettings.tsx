bd4fbdfe4c9bede5f422b3eb410e382764f96fb3
Sam Gabbard <samgabbard11@gmail.com>
2023-04-15 15:27:50 -0700

    set up vertical tab template for account settings page

--- client/src/features/accountSettings/AccountSettings.tsx
+++ client/src/features/accountSettings/AccountSettings.tsx
@@ -1,11 +1,76 @@
-import { Typography } from "@mui/material";
+import { useState } from "react";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3, width:'1000px' }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
 
 function AccountSettings() {
-    return (
-        <>
-        <Typography>Account Settings</Typography>
-        </>
-    )
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab label="My Profile" {...a11yProps(0)} />
+        <Tab label="Appearance" {...a11yProps(1)} />
+        <Tab label="Security" {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Profile settings here...
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Appearance settings here...
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        email change and password change here...
+      </TabPanel>
+    </Box>
+  );
 }
 
 export default AccountSettings;
