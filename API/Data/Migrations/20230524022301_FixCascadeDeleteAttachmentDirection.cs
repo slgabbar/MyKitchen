e0f46155c83ca1436e55e538757c26059d73c7e8
Sam Gabbard <samgabbard11@gmail.com>
2023-05-23 19:23:25 -0700

    updated casacde delete on attachment

---
+++ API/Data/Migrations/20230524022301_FixCascadeDeleteAttachmentDirection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCascadeDeleteAttachmentDirection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attachment_AttachmentBlob_AttachmentKey",
+                table: "Attachment");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AttachmentBlob_Attachment_AttachmentKey",
+                table: "AttachmentBlob",
+                column: "AttachmentKey",
+                principalTable: "Attachment",
+                principalColumn: "AttachmentKey",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AttachmentBlob_Attachment_AttachmentKey",
+                table: "AttachmentBlob");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attachment_AttachmentBlob_AttachmentKey",
+                table: "Attachment",
+                column: "AttachmentKey",
+                principalTable: "AttachmentBlob",
+                principalColumn: "AttachmentKey",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
