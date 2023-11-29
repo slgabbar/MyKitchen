7e61145144d69c55e5a9193c722a873fd2230549
Sam Gabbard <samgabbard11@gmail.com>
2023-11-28 20:35:21 -0800

    starting simple prep time and cook tim edisplay

--- client/src/features/recipe/Recipe.tsx
+++ client/src/features/recipe/Recipe.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Typography } from "@mui/material";
+import { Box, Button, Card, Divider, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import agent from "../../app/api/agent";
@@ -31,16 +31,41 @@ function Recipe() {
                     Back to My Recipes
                 </Button>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box sx={{ mb:2, alignSelf: 'center' }}>
                 <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
                     {recipe?.title}
                 </Typography>
             </Box>
-            <Divider sx={{mb:2, mt:1}} />
-            <Box sx={{ flexGrow: 1, mb: 2 }}>
+            <Box sx={{ mb: 2, alignSelf: 'center' }}>
+                <Typography>
+                    {recipe?.description}
+                </Typography>
+            </Box>
+            <Box display='flex' sx={{ justifyContent: 'center' }}>
+                <Card variant="outlined" sx={{ px:2, display: 'flex', justifyContent: 'center' }}>
+                    <Box sx={{ m: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Typography variant="overline">Yields:</Typography>
+                        <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>--</Typography>
+                    </Box>
+                    <Divider sx={{ mx: 2 }} orientation="vertical" variant="middle" flexItem />
+                    <Box sx={{ m: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Typography variant="overline">Prep Time:</Typography>
+                        <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>--</Typography>
+                    </Box>
+                    <Divider sx={{ mx: 2 }} orientation="vertical" variant="middle" flexItem />
+                    <Box sx={{ m: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Typography variant="overline">Cook Time:</Typography>
+                        <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>--</Typography>
+                    </Box>
+                    <Divider sx={{ mx: 2 }} orientation="vertical" variant="middle" flexItem />
+                    <Box sx={{ m: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Typography variant="overline">Total Time:</Typography>
+                        <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>--</Typography>
+                    </Box>
+                </Card>
             </Box>
         </Box>
-  );
+    );
 }
 
 export default Recipe; 
